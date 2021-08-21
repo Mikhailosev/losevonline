c0eae86fa5e63828a28df87decf19cb7d6857cc4
mikhailosev <mikhailosev@gmail.com>
2021-08-21 14:48:39 +0300

    test lint-staged

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      test
+    </>
+  )
 }
 export default MyApp
