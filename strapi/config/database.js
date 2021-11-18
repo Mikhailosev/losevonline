bc970c6a751f5007d72d399702ca069d26393733
mikhailosev <mikhailosev@gmail.com>
2021-11-18 19:31:49 +0300

    added lightbox and config for different envs

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,20 +1,40 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), 
+module.exports = ({ env }) => {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'strapi'),
+            username: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'strapi'),
+            schema: env('DATABASE_SCHEMA', 'public'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-})
+    }
+  } else {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
+      }
+    }
+  }
+}
--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,11 +1,26 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url:'http://213.139.211.77/api',
-  admin: {
-    autoOpen:false,
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '93825d2c44e277852331dc265c9c7b2f'),
-    },
-  },
-})
+module.exports = ({ env }) => {
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+      url: 'http://213.139.211.77/api',
+      admin: {
+        autoOpen: false,
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', '93825d2c44e277852331dc265c9c7b2f'),
+        },
+      },
+    }
+  } else {
+    return {
+      host: env('HOST', '0.0.0.0'),
+      port: env.int('PORT', 1337),
+      admin: {
+        auth: {
+          secret: env('ADMIN_JWT_SECRET', '93825d2c44e277852331dc265c9c7b2f'),
+        },
+      },
+    }
+  }
+}
